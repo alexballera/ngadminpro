e398c6ef57aa9665d236c1c1680239f746020c3d
Alex Ballera <alexballera@gmail.com>
2020-05-25 23:01:00 -0300

    Agregado componente de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
@@ -20,7 +21,7 @@ const routes: Routes = [
     ]
   },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent },
 ];
 

210c682780c812804214eabf7f7f9cf65b151c41
Alex Ballera <alexballera@gmail.com>
2020-06-11 09:28:00 -0300

    Inputs

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -8,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncrementadorComponent implements OnInit {
 
-  progreso: number = 50;
+  @Input() progreso: number = 50;
 
-  leyenda: string = 'Leyenda';
+  @Input('nombre') leyenda: string = 'Leyenda';
 
-  constructor() { }
+  constructor() {
+    console.log('Leyenda ', this.leyenda);
+    // console.log('Leyenda ', this.progreso)
+  }
 
   ngOnInit(): void {
   }

19bc5a865f626c93daa7cad8197e9da29b54ab83
Alex Ballera <alexballera@gmail.com>
2020-06-19 21:52:42 -0300

    Teriminado input

--- src/app/components/grafico-donas/grafico-donas.component.ts
+++ src/app/components/grafico-donas/grafico-donas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-donas',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grafico-donas.component.scss']
 })
 export class GraficoDonasComponent implements OnInit {
+  @Input() chartLabels: string[] = [];
+  @Input() chartData: number[] = [];
+  @Input() chartType: string = '';
 
   constructor() { }
 
--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -9,14 +9,32 @@ import { MultiDataSet, Label } from 'ng2-charts';
   ]
 })
 export class Graficas1Component implements OnInit {
-  // Doughnut
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: MultiDataSet = [
-    [350, 450, 100],
-    [50, 150, 120],
-    [250, 130, 70],
-  ];
-  public doughnutChartType: ChartType = 'doughnut';
+  graficos: any = {
+    grafico1: {
+      labels: ['Con Frijoles', 'Con Natilla', 'Con tocino'],
+      data:  [24, 30, 46],
+      type: 'doughnut',
+      leyenda: 'El pan se come con'
+    },
+    grafico2: {
+      labels: ['Hombres', 'Mujeres'],
+      data:  [4500, 6000],
+      type: 'doughnut',
+      leyenda: 'Entrevistados'
+    },
+    grafico3: {
+      labels: ['Si', 'No'],
+      data:  [95, 5],
+      type: 'doughnut',
+      leyenda: '¿Le dan gases los frijoles?'
+    },
+    grafico4: {
+      labels: ['No', 'Si'],
+      data:  [85, 15],
+      type: 'doughnut',
+      leyenda: '¿Le importa que le den gases?'
+    },
+  };
 
   constructor() { }
 

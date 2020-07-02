e26fc5e9aca2d628a65441f98ddd4240d32620ad
Alex Ballera <alexballera@gmail.com>
2020-07-02 16:55:28 -0300

    Agregada funcionalidad para check

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -13,11 +13,23 @@ export class AccountSettingsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  cambiarColor(theme: string) {
+  cambiarColor(theme: string, link: any) {
+
+    this.aplicarCheck(link);
 
     const url = `assets/css/colors/${theme}.css`;
 
     this.document.getElementById('theme').setAttribute('href', url);
+  }
+
+  aplicarCheck(link: any) {
+    const selectores: any = this.document.getElementsByClassName('selector');
+
+    for (const ref of selectores) {
+      ref.classList.remove('working');
+    }
+
+    link.classList.add('working');
   }
 
 }

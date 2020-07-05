a04010f0ecb66e6522d581d710e73710e9d961e3
Alex Ballera <alexballera@gmail.com>
2020-07-05 16:52:21 -0300

    Agregada ruta rxjs

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -14,6 +14,8 @@ export class SidebarService {
         { titulo: 'ProgressBar', url: '/progress', },
         { titulo: 'Gráficas', url: '/graficas1', },
         { titulo: 'Configuraciones', url: '/account-settings', },
+        { titulo: 'Promesas', url: '/promesas', },
+        { titulo: 'RXJS', url: '/rxjs', },
       ]
     }
   ];
